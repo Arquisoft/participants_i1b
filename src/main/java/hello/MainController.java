cddd8bd9ba44f21c094f5d5914d74fe20d32cfce
Ronce 96 <Ronce 96>
2017-02-16 13:06:53 +0100

    The application generates a new citizen with the information of the form and show his details

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -3,8 +3,14 @@ package hello;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,18 +20,23 @@ import model.Citizen;
 public class MainController {
 
     @RequestMapping("/")
-    public String landing(Model model) {
-    	model.addAttribute("citizen", new Citizen("pablo"
-    			,"roncero"
-    			,new Date()
-    			,""
-    			,""
-    			,""
-    			,""
-    			,0
-    			)
-    	);
+    public String landing() {
         return "index";
+    }
+    
+    @GetMapping("/login")
+    public String login(HttpServletRequest request, Model model){
+    	String login = request.getParameter("login");
+    	String password =  request.getParameter("password");
+    	
+    	Citizen citizen = new Citizen(login, login, new Date(), login, login, login, login, 1);
+    	model.addAttribute("citizen",citizen);
+    	return "info";
+    }
+    
+    @PostMapping("/info")
+    public String mostrarInfo(Model model){
+    	return "info";
     }
     
     @RequestMapping("/hola")
