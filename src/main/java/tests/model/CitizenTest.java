11a2fd5eb0170c8104c3f3b27b4ad1de27ebe5e7
David Villamil <davillafer>
2017-02-23 01:55:24 +0100

    Tests added (Navigation done but commented)

---
+++ src/main/java/tests/model/CitizenTest.java
@@ -0,0 +1,43 @@
+package tests.model;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Citizen;
+
+public class CitizenTest {
+
+	private Citizen ct1;
+
+	@SuppressWarnings("deprecation")
+	@Before
+	public void setUp() throws Exception {
+		ct1 = new Citizen("userTest", "asw99", new Date(1981,12,01,00,00) , "userTest@mail.com", "1681U", "calle pos", "GER", 120);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void basicMethodsTest() {
+		assertEquals( ct1.getFirstName()			,"userTest");
+		assertEquals( ct1.getLastName()				,"asw99");
+		assertEquals( ct1.getBirthday()				,new Date(1981,12,01,00,00));
+		assertEquals( ct1.getEmail()				,"userTest@mail.com");
+		assertEquals( ct1.getNif()					,"1681U");
+		assertEquals( ct1.getAddress()				,"calle pos");
+		assertEquals( ct1.getNationality()			,"GER");
+		assertEquals( ct1.getpollingStationCode()	,120);
+		
+	}
+	
+	@Test
+	public void testToString() {
+	    Assert.assertEquals("Citizen [id=" + ct1.getId() + ", firstName=" + ct1.getFirstName() + ", lastName=" + ct1.getLastName() + ", birthday=" + ct1.getBirthday()
+				+ ", email=" + ct1.getEmail() + ", nif=" + ct1.getNif() + ", address=" + ct1.getAddress() + ", nationality=" + ct1.getNationality()
+				+ ", pollingStationCode=" + ct1.getpollingStationCode() + ", password=" + ct1.getPassword() + "]", ct1.toString());
+	}
+}
---
+++ src/main/java/tests/webPages/NavigationTest.java
@@ -0,0 +1,83 @@
+package tests.webPages;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import model.Citizen;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class NavigationTest {
+
+	private WebEnvironment port = WebEnvironment.RANDOM_PORT;
+	private TestRestTemplate rt;
+	private Citizen ct1;
+	private URL base;
+
+	@Autowired
+	private WebApplicationContext webapp;
+
+	@Before(value = "")
+	public void setUp() {
+		rt = new TestRestTemplate();
+		try {
+			this.base = new URL("http://localhost:" + port + "/");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		MockMvcBuilders.webAppContextSetup(this.webapp).build();
+	}
+
+	@Test
+	public void loginPageTest() throws Exception {
+//		ResponseEntity<String> response = rt.getForEntity(base.toString(), String.class);
+//		assertThat(response.getBody(), containsString("Login page"));
+	}
+
+	@Test
+	public void testLoginActionRIGHT() {
+//		RequestBuilder requestLogin = 
+//				post("/login").param("login", ct1.getEmail()).param("password", ct1.getPassword());
+//
+//		assertThat(rt
+//		        .postForObject("http://localhost:" + port + "/login", requestLogin, String.class)
+//		        .contains(ct1.getFirstName()));
+//		assertThat(rt
+//		        .postForObject("http://localhost:" + port + "/login", requestLogin, String.class)
+//		        .contains(ct1.getLastName()));
+//		assertThat(rt
+//		        .postForObject("http://localhost:" + port + "/login", requestLogin, String.class)
+//		        .contains(ct1.getEmail()));
+//		assertThat(rt
+//		        .postForObject("http://localhost:" + port + "/login", requestLogin, String.class)
+//		        .contains(ct1.getNationality()));
+	}
+
+	@Test
+	public void testLoginActionWRONG() {
+//		RequestBuilder requestLogin = 
+//				post("/login").param("login", "errormail").param("password", "errorPass");
+//
+//		assertThat(rt
+//		        .postForObject("http://localhost:" + port + "/login", requestLogin, String.class)
+//		        .contains("Error 404 user not found!"));
+	}
+}
